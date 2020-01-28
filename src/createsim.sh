35be5598276084dc6912e6137060322c51db0ef9
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-01-28 21:47:52 +0100

    Updated version of createsim.sh . Not tested yet.

--- src/createsim.sh
+++ src/createsim.sh
@@ -5,14 +5,19 @@
 #
 ######################################################################
 
+###########_PATH_#########################
+DIR_ROOT=../
+DIR_SRC=$DIR_ROOT/src/
+DIR_SIM=$DIR_ROOT/cache/
+DIR_PAR=$DIR_ROOT/param/
+PATH_GENERATOR=$DIR_SRC/SimFiles_generator.R
+
 ###########_variables_####################
-pathbase=$(realpath ./)
-pathdest=$(realpath ./Simulation)
 BASE_NAME=Simul
 TYPE=Domestication
 DATE=$(date +%F)
 Compt=1
-NEWSIM=$pathdest/$BASE_NAME-$TYPE-${DATE}-$Compt
+NEWSIM=$DIR_SIM/$BASE_NAME-$TYPE-${DATE}-$Compt
 
 #default values
 SIZE_POP=1000
@@ -27,8 +32,10 @@ DOMEST=FALSE
 SCENAR_MODIF=FALSE
 PARAM_MODIF=FALSE
 OPTION=I
-CRS=FALSE
 TIMEBOT=2800
+TEMPLATE_PARAM=$DIR_PAR/param0.txt
+TEMPLATE_EXTPARAM=$DIR_PAR/extparam0.txt
+
 #########_thelp_function_######################
 show_help() {
 	echo " -Z    switch the strength of selection to 0 during all the time of the simulation"
@@ -40,20 +47,19 @@ show_help() {
 	echo " -o <value>    modify the output rate  Default : $OUTPUT_GEN "
 	echo " -r <value>    modify the number of replicates  Default : $NB_REP "
 	echo " -k <value>	 modify the stregth of the bottleneck "
-	echo " -C 	 enable the switch in selection regime"
-	echo " -E    allow to modify the others parameters if needed"
 	echo " -m <value>    change the mutation rate Default : $MUT_RATE "
 	echo " -c <value>    change the initial connection probability of the network Default : $INIT_CONNECT"
 	echo " -B <value>    change the number of generations before the bottleneck Default : 12000"
 	echo " -A <value>	 change the number of generations after the bottleneck  Default : 6200"
-	echo " -T    allow to modify the scenario of domestication "
 	echo " -D 	 register the launchfile in bigmem in order to allow the parrallel launch of all your simulation"
+	echo " -P <value>    template parameter file"
+	echo " -p <value>    template extended parameter file"
 	echo " -h    show help"
 }
 
 	
 #######_options_##########################
-while getopts "h?:N:o:r:m:c:A:B:k:S::TGEDCLIZ" opt; do 
+while getopts "h?:N:o:r:m:c:A:B:k:S::TGEDCLIZP:p:" opt; do 
 case $opt in
 	N)		
 	SIZE_POP=$OPTARG >&2
@@ -69,10 +75,6 @@ case $opt in
 	;;
 	G)		
 	OPTION=$OPTION" -bot" >&2
-	;;
-	C)		
-	CRS=TRUE 
-	OPTION=$OPTION" -crs">&2
 	;;
 	o)		
 	OUTPUT_GEN=$OPTARG >&2
@@ -107,85 +109,70 @@ case $opt in
 	Z)      
 	OPTION=$OPTION" -zero_selec" >&2
 	;;
+	P)
+	TEMPLATE_PARAM=$OPTARG >&2
+	;;
+	p)
+	TEMPLATE_EXTPARAM=$OPTARG >&2
+	;;
 	h|\?)
         show_help
         exit 0
     ;;
 	\?)
-	echo "option invalide, aide disposible via argument -h " >&2
+	echo "Invalid option, help avilable via the argument -h" >&2
 	;;
 esac
 done
 OPTION=$OPTION" -mu $MUT_RATE"
+
+
 ###########_actions_#######################
 
-#creation du fichier contenant le pipeline + dossier d'entrée et sortie + sortie pour R
+# Creating the directory including the simulation pipeline + input/output files
 
 
-
-if [ ! -d "$pathdest" ] ; then
-mkdir $pathdest
+if [ ! -d "$DIRSIM" ] ; then
+mkdir $DIRSIM
 fi
 
 
 while [ -d "$NEWSIM" ] ; do
 Compt=$(($Compt +1 ))
-NEWSIM=$pathdest/$BASE_NAME-$TYPE-${DATE}-$Compt
+NEWSIM=$DIRSIM/$BASE_NAME-$TYPE-${DATE}-$Compt
 done
 
+SIMPARAM=$NEWSIM/param.txt
+SIMEXTPARAM=$NEWSIM/extparam.txt
+
 mkdir $NEWSIM
-cp $pathbase/param.txt $NEWSIM
+cp $TEMPLATE_PARAM $SIMPARAM
+cp $TEMPLATE_EXTPARAM $SIMEXTPARAM
 touch $NEWSIM/launchfile.sh 
 chmod a+x $NEWSIM/launchfile.sh 
 mkdir $NEWSIM/out-in_Fold
 
-#modification du fichier param.txt
-echo "SIMUL_OUTPUT  $OUTPUT_GEN" >> $NEWSIM/param.txt
-echo "INIT_PSIZE  $SIZE_POP" >> $NEWSIM/param.txt
-echo "INIT_CONNECT $INIT_CONNECT" >> $NEWSIM/param.txt
-echo "GENET_MUTRATES  $MUT_RATE" >> $NEWSIM/param.txt
+# Changes in the parameter file
+echo "SIMUL_OUTPUT  $OUTPUT_GEN"  >> $SIMPARAM
+echo "INIT_PSIZE  $SIZE_POP"      >> $SIMPARAM
+echo "INIT_CONNECT $INIT_CONNECT" >> $SIMPARAM
+echo "GENET_MUTRATES  $MUT_RATE"  >> $SIMPARAM
 
-#creation et remplissage du fichier extparam.txt
-echo "Nombre_replicat   $NB_REP" > $NEWSIM/extparam.txt
-echo "Règles de codage :" >> $NEWSIM/extparam.txt
-echo "1 = gène stable avec même objectif avant et après domestication" >> $NEWSIM/extparam.txt
-echo "2 = gène plastique avec pour valeur x = valeur de l'environnement " >> $NEWSIM/extparam.txt
-echo "3 = gène nouvellement stable (changement d'objectif quand on passe de 1 à 3 )" >> $NEWSIM/extparam.txt
-echo "4 = gène plastique avec pour valeur x = 1-valeur de l'environnement" >> $NEWSIM/extparam.txt
-echo "0 = gène non selectionné = gène neutre " >> $NEWSIM/extparam.txt
-echo "NB : il faut autant de chiffre qu'il y a de gènes " >> $NEWSIM/extparam.txt
-echo "Scenario_part1 2 1 1 1 1 1 1 2 2 2 4 4 4 0 0 0 0 0 0 0 0 0 0 0 0" >> $NEWSIM/extparam.txt
-echo "$CRS"
+# Changes in the extended parameter file
 
-if [ "$CRS" = "TRUE" ]; then	# if CRS = T alors le scenario 1 et le scenario 2 sont différent, sinon le scenario1=scenario2
-echo "Scenario_part2 2 1 1 3 3 0 0 3 0 2 4 3 0 3 3 3 3 0 0 0 0 0 0 0 0" >> $NEWSIM/extparam.txt ; 
-else  
-echo "Scenario_part2 2 1 1 1 1 1 1 2 2 2 4 4 4 0 0 0 0 0 0 0 0 0 0 0 0" >> $NEWSIM/extparam.txt ; 
-fi	
-echo "nb generations bottleneck = $TIMEBOT" >> $NEWSIM/extparam.txt
-echo "OPTIONS = $OPTION" >> $NEWSIM/extparam.txt
+echo "Nombre_replicat   $NB_REP"            >> $SIMEXTPARAM
+echo "nb generations bottleneck = $TIMEBOT" >> $SIMEXTPARAM
+echo "OPTIONS = $OPTION"                    >> $SIMEXTPARAM
+
 ###
 OPTION=$OPTION" -dir $NEWSIM"
 
 
-# affichage et edition des fichier de paramétrage si option -T ou -E activé
-cd $NEWSIM
-if [ "$PARAM_MODIF" = TRUE ]; then
-nano param.txt
-fi
-if [ "$SCENAR_MODIF" = TRUE ]; then
-nano extparam.txt 
-fi
-
-#lancement de la géneration des fichier d'entrée + création du launchfile
-echo "création de l'environnement"
-cd $pathbase/Script
-echo "$OPTION"
-./SimFiles_generator.R $OPTION
+# Creating the simulation environment
+#~ cd $pathbase/Script
+Rscript $PATH_GENERATOR $OPTION # TODO: output in the right directory
 #register the lauchfile.sh of newsim in bigmem.sh
-if [ "$BIG_LAUNCH" = TRUE ]; then
-cat "$NEWSIM/launchfile.sh" >> $pathbase/bigmem.sh
-echo " le fichier de lancement de simulation à été transféré dans bigmem"
-fi
-
-echo "fin de tache"
+#~ if [ "$BIG_LAUNCH" = TRUE ]; then
+#~ cat "$NEWSIM/launchfile.sh" >> $pathbase/bigmem.sh
+#~ echo " le fichier de lancement de simulation à été transféré dans bigmem"
+#~ fi
