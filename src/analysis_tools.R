6cfe8017475d6d0f5c5f63d0a17eb1cebb88c4db
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-09-11 11:07:00 +0200

    Pasting the get.Ndyn function in analysis_tools.R + minimal comments on the purporse of the functions.

--- src/analysis_tools.R
+++ src/analysis_tools.R
@@ -1,8 +1,8 @@
 # Various functions to analyse the simulation results
 
+# Applies the function FUN (typically, mean or var) to each element of the matrix/data.frame stored in a vector of files
 replicate.apply <- function(files, FUN=mean, ...) {
 	library(abind)
-	# Applies the function FUN (typically, mean or var) to each element of the data frame stored in files
 	tt <- lapply(files, function(ff) read.table, header=TRUE)
 	# better checking if the data base is consistent
 	stopifnot(length(unique(sapply(tt, nrow))) == 1, length(unique(sapply(tt, ncol))) == 1)
@@ -10,6 +10,24 @@ replicate.apply <- function(files, FUN=mean, ...) {
 	apply(arr, 1:2, FUN)
 }
 
+# Reconstructs the dynamics of the population size N from a replicate directory 
+# (takes a bit of time due to the number of files)
+get.Ndyn <- function(repdir) {
+	ll <- list.files(path=repdir, pattern="^param.*\\.txt$", full.names=TRUE)
+	myN <- sapply(ll, function(ff) { 
+		if (length(grep("INIT_PSIZE", readLines(ff))) == 0) 
+			return(NA)
+		else {
+			pp <- read.param(ff)
+			return(pp[["INIT_PSIZE"]])
+		}
+		})
+	myN <- na.omit(myN)[cumsum(!is.na(myN))] # Replace NAs by the last non-NA
+	names(myN) <- regmatches(ll, regexpr(ll, pattern="gen(\\d+)"))
+	myN
+}
+
+# Detects the begining and the end of the bottleneck(s?) from the dynamics of N
 bottleneck.detect <- function(Ndyn) {
 	# Returns the generation number if Ndyn is named with generations, otherwise the index
 	if (is.null(names(Ndyn)) names(Ndyn) <- as.character(seq_along(Ndyn))
@@ -20,6 +38,7 @@ bottleneck.detect <- function(Ndyn) {
 	list(begin=as.numeric(names(Ndyn)[bottle.begin]), end=as.numeric(names(Ndyn)[bottle.end]))
 }
 
+# Detects the generation number(s?) at which the selection regime changes
 selectionchange.detect <- function(out.table) {
 	# The task is difficult, has to rely on some assumptions 
 	# The algo looks for stable optima that changes from time to time
@@ -36,11 +55,13 @@ selectionchange.detect <- function(out.table) {
 	return(out.table$Gen[changes.domestic[[1]]])
 }
 
+# Adds the bottleneck mark in a figure
 bottleneck.plot <- function(Ndyn, y=0, code=3, angle=90, ...) {
 	bttl <- bottleneck.detect(Ndyn)
 	arrows(x0=bbtl$begin, x1=bbtl$end, y0=y, code=code, angle=angle, ...)
 }
 
+# Adds the selection mark in a figure
 selectionchange.plot <- function(out.table, y=0, pch=25, col="black", bg="red", ...) {
 	sel <- selectionchange.detect(out.table)
 	points(x=sel, y=y, pch=pch, col=col, bg=bg, ...)
