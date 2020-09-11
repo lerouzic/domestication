5ac657fcfbb049ef5d2ec22b56edf7920a2aff66
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-09-11 14:47:51 +0200

    figA complete + many bug fixes in analysis_tools.R.

--- results/figA.R
+++ results/figA.R
@@ -2,53 +2,56 @@
 
 # First figure: dynamics of the effective pop size and average fitness +/- sd for a random population
 
-source("../src/makeparam_functions.R")
+library(parallel)
+mc.cores <- min(12, detectCores()-1)
+
+source("../src/analysis_tools.R")
 
 out.dir <- "../cache/simDefault"
-target.rep <- NA # If NA, pick a random replicate
+target.reps <- NA # If NA, pick all replicates
+random.rep <- NA # If NA, chose a random rep (otherwise specify it)
 
-get.N.dyn <- function(repdir) {
-	ll <- list.files(path=repdir, pattern="^param.*\\.txt$", full.names=TRUE)
-	myN <- sapply(ll, function(ff) { 
-		if (length(grep("INIT_PSIZE", readLines(ff))) == 0) 
-			return(NA)
-		else {
-			pp <- read.param(ff)
-			return(pp[["INIT_PSIZE"]])
-		}
-		})
-	myN <- na.omit(myN)[cumsum(!is.na(myN))] # Replace NAs by the last non-NA
-	names(myN) <- regmatches(ll, regexpr(ll, pattern="gen(\\d+)"))
-	myN
+if (is.na(target.reps)) {
+	target.reps <- list.dirs(out.dir, full.names=TRUE, recursive=FALSE)
+	target.reps <- target.reps[grep(basename(target.reps), pattern="rep-\\d+")]
 }
 
+if (length(target.reps %in% list.dirs(out.dir, full.names=TRUE, recursive=FALSE)) == 0) stop("Unable to find the results in ", out.dir, ".")
 
-
-if (is.na(target.rep)) {
-	rep.dirs <- list.dirs(out.dir, full.names=TRUE, recursive=FALSE)
-	if (length(rep.dirs) == 0) stop("Unable to find the results in ", out.dir, ".")
-	target.rep <- sample(rep.dirs, 1)
+if (is.na(random.rep)) {
+	random.rep <- sample(target.reps, 1)
 }
 
-out.files <- list.files(pattern="out.*", path=target.rep, full.names=TRUE)
+out.files <- list.files(pattern="out.*", path=target.reps, full.names=TRUE)
 
 stopifnot(length(out.files)>0)
 
-out.mean <- replicate.apply(out.files, mean)
-out.var  <- replicate.apply(out.files, var)
+tt <- mclapply(out.files, read.table, header=TRUE, mc.cores=4) # read.tables on many cores is useless, probably limited by disk speed
+out.mean <- replicate.mean(tt)
+out.var  <- replicate.var(tt)
 
-mfit <- out[,"MFit"]
-vfit <- out[,"VFit"]
-gen <- as.numeric(out[,"Gen"])
+mfit <- out.mean[,"MFit"]
+vfit <- out.mean[,"VFit"]
+gen <- out.mean[,"Gen"]
 
-N <- get.N.dyn(target.rep)[gen+1]
+N <- get.Ndyn(random.rep)[gen]
+N <- c(N[1], N) # just to deal in a bug in the naming of the parameter files
 
-plot(gen, N, type="l", xlim=range(gen), ylim=c(0, max(N)))
+pdf("figA.pdf", width=8, height=4)
+
+layout(t(1:2))
+
+plot(gen, N, type="l", xlim=range(gen), ylim=c(0, max(N)), ylab="Population size", xlab="Generations")
 lines(gen, N/(1+4*vfit/mfit/mfit), col="blue")
-
+bottleneck.plot(N, y=1, lwd=2)
+selectionchange.plot(out.mean, y=1, cex=1.5)
+legend("bottomright", lty=1, col=c("black","blue"), legend=c("N", expression(N[e])), bty="n")
 
 plot(NULL, xlab="Generations", ylab="Fitness", xlim=range(gen), ylim=c(0,1))
-lines(gen, mfit+sqrt(vfit), lty=1, col="blue")
-lines(gen, mfit-sqrt(vfit), lty=1, col="blue")
+#~ lines(gen, mfit+sqrt(vfit), lty=1, col="blue")
+#~ lines(gen, mfit-sqrt(vfit), lty=1, col="blue")
 lines(gen, mfit, lwd=2)
+bottleneck.plot(N, y=1, lwd=2)
+selectionchange.plot(out.mean, y=1, cex=1.5)
 
+dev.off()
--- src/analysis_tools.R
+++ src/analysis_tools.R
@@ -1,13 +1,39 @@
 # Various functions to analyse the simulation results
 
-# Applies the function FUN (typically, mean or var) to each element of the matrix/data.frame stored in a vector of files
-replicate.apply <- function(files, FUN=mean, ...) {
-	library(abind)
-	tt <- lapply(files, function(ff) read.table, header=TRUE)
+library(abind)
+
+source("../src/makeparam_functions.R") # dubious path management...
+
+# Applies the function FUN (typically, mean or var) to each element of the list of matrix/data.frames 
+replicate.apply <- function(tt, FUN=mean, ...) {
 	# better checking if the data base is consistent
 	stopifnot(length(unique(sapply(tt, nrow))) == 1, length(unique(sapply(tt, ncol))) == 1)
-	arr <- do.call(abind, tt, along=3)
-	apply(arr, 1:2, FUN)
+	arr <- do.call(abind, c(tt, list(along=3)))
+	ans <- apply(arr, 1:2, FUN, ...)
+	ans
+}
+
+# This is faster than replicate.apply(tt, mean)
+replicate.mean <- function(tt) {
+	stopifnot(length(unique(sapply(tt, nrow))) == 1, length(unique(sapply(tt, ncol))) == 1)
+	arr <- do.call(abind, c(tt, list(along=3)))
+	rowMeans(arr, dims=2)
+}
+
+# This is faster than replicate.apply(tt, var)
+replicate.var <- function(tt) {
+	# This rowVars function comes from https://stat.ethz.ch/pipermail/r-help/2006-April/103001.html
+	# Author: David Brahm
+	.rowVars <- function(x, na.rm=FALSE, dims=1, unbiased=TRUE, SumSquares=FALSE, twopass=FALSE) {
+		if (SumSquares) return(rowSums(x^2, na.rm, dims))
+		N <- rowSums(!is.na(x), FALSE, dims)
+		Nm1 <- if (unbiased) N-1 else N
+		if (twopass) {x <- if (dims==0) x - mean(x, na.rm=na.rm) else sweep(x, 1:dims, rowMeans(x,na.rm,dims))}
+		(rowSums(x^2, na.rm, dims) - rowSums(x, na.rm, dims)^2/N) / Nm1
+	}
+	stopifnot(length(unique(sapply(tt, nrow))) == 1, length(unique(sapply(tt, ncol))) == 1)
+	arr <- do.call(abind, c(tt, list(along=3)))
+	.rowVars(arr, dims=2)
 }
 
 # Reconstructs the dynamics of the population size N from a replicate directory 
@@ -23,18 +49,18 @@ get.Ndyn <- function(repdir) {
 		}
 		})
 	myN <- na.omit(myN)[cumsum(!is.na(myN))] # Replace NAs by the last non-NA
-	names(myN) <- regmatches(ll, regexpr(ll, pattern="gen(\\d+)"))
+	names(myN) <- sapply(regmatches(ll, regexec(ll, pattern="gen(\\d+)")), "[", 2)
 	myN
 }
 
 # Detects the begining and the end of the bottleneck(s?) from the dynamics of N
 bottleneck.detect <- function(Ndyn) {
 	# Returns the generation number if Ndyn is named with generations, otherwise the index
-	if (is.null(names(Ndyn)) names(Ndyn) <- as.character(seq_along(Ndyn))
-	stopifnot(length(Ndyn) < 3)
+	if (is.null(names(Ndyn))) names(Ndyn) <- as.character(seq_along(Ndyn))
+	stopifnot(length(Ndyn) > 3)
 	if (length(unique(Ndyn)) > 10) warning("Probably too many changes in N to detect a bottleneck")
-	bottle.begin <- which(diff(Ndyn) < 0)
-	bottle.end <- which(diff(Ndyn) > 0)
+	bottle.begin <- which(diff(Ndyn) < 0) + 1
+	bottle.end <- which(diff(Ndyn) > 0) + 1
 	list(begin=as.numeric(names(Ndyn)[bottle.begin]), end=as.numeric(names(Ndyn)[bottle.end]))
 }
 
@@ -43,11 +69,10 @@ bottleneck.detect <- function(Ndyn) {
 # The output is two characters, one before and one after domestication (even for constant genes, which are cc for consistency). 
 selectionregime.detect <- function(out.table) {
 	# Assumes only one change in the selection regime
-	opts <- out.table[,grepl(rownames(out.table), pattern="theta")]
+	opts <- out.table[,grepl(colnames(out.table), pattern="FitOpt")]
 	stopifnot (length(opts) > 0)
-	
-	stability <- lapply(opts function(x) { ans <- ifelse(diff(x) == 0, "s", "p"); ans[x[-1] == 0] <- "n"; paste(rle(ans)$values, collapse="") } 
-	category <- sapply(stability, function(x) if (x == "s") "cc" else if (x == "p") "pp" else if (x == "n") "n" else x)
+	stability <- lapply(as.data.frame(opts), function(x) { ans <- ifelse(diff(x) == 0, "s", "p"); ans[x[-1] == 0] <- "n"; paste(rle(ans)$values, collapse="") } )
+	category <- sapply(stability, function(x) if (x == "s") "cc" else if (x == "p") "pp" else if (x == "n") "nn" else if (x=="nps") "ns" else if (x == "sps") "ss" else x)
 	return(category)
 }
 
@@ -56,17 +81,16 @@ selectionchange.detect <- function(out.table) {
 	# The task is difficult, has to rely on some assumptions 
 	# The algo looks for stable optima that changes from time to time
 	# out.file can be the mean over replicates or a single file. Probably works better with a single file
-	
 	selcat <- selectionregime.detect(out.table)
-	changes.domestic <- lapply(opts[selcat == ss], function(x) which(diff(x)!=0))
-	if (!do.call(all.equal, changes.domestic)) stop("The selection change history looks different for domestic genes.")
-	return(out.table$Gen[changes.domestic[[1]]])
+	changes.domestic <- as.matrix(apply(out.table[,names(selcat)[selcat == "ss"]], 2, function(x) which(diff(x)!=0)))
+	if (!all(apply(changes.domestic, 2, function(x) length(unique(x))==1))) stop("The selection change history looks different for domestic genes.")
+	return(out.table[1+changes.domestic[1,],"Gen"])
 }
 
 # Adds the bottleneck mark in a figure
-bottleneck.plot <- function(Ndyn, y=0, code=3, angle=90, ...) {
+bottleneck.plot <- function(Ndyn, y=0, code=3, angle=90, length=0.1, ...) {
 	bttl <- bottleneck.detect(Ndyn)
-	arrows(x0=bbtl$begin, x1=bbtl$end, y0=y, code=code, angle=angle, ...)
+	arrows(x0=bttl$begin, x1=bttl$end, y0=y, code=code, angle=angle, length=length, ...)
 }
 
 # Adds the selection mark in a figure
