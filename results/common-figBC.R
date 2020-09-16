1c0987bd2af95734eedc1eb12d68344dfba8eb22
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-09-16 14:22:36 +0200

    Some functions moved from commonBC to analysis_tools, because of reuse in other scripts. + reaction norm functions in analysis_tools.

--- results/common-figBC.R
+++ results/common-figBC.R
@@ -5,16 +5,7 @@ source("../src/analysis_tools.R")
 library(parallel)
 mc.cores <- min(12, detectCores()-1)
 
-mean.sim <- function(out.dir, max.reps=5) {
-	out.reps <- list.dirs(out.dir, full.names=TRUE, recursive=FALSE)
-	out.files <- list.files(pattern="out.*", path=out.reps, full.names=TRUE)
-
-	tt <- mclapply(out.files[1:(if (detectCores() > 24 || length(out.files) < max.reps) length(out.files) else max.reps)], read.table, header=TRUE, mc.cores=4) # read.tables on many cores is useless, probably limited by disk speed
-	ans <- replicate.mean(tt)
-	rm(tt)
-	gc()
-	return(ans)
-}
+mean.sim <- function(x) mean.sim(x, max.reps=if (detectCores() > 23 Inf) else 5) # For tests 
 
 onerep <- function(out.dir) list.dirs(out.dir, full.names=TRUE, recursive=FALSE)[1]
 
--- src/analysis_tools.R
+++ src/analysis_tools.R
@@ -1,8 +1,10 @@
 # Various functions to analyse the simulation results
 
 library(abind)
+library(parallel)
 
 source("../src/makeparam_functions.R") # dubious path management...
+
 
 # Applies the function FUN (typically, mean or var) to each element of the list of matrix/data.frames 
 replicate.apply <- function(tt, FUN=mean, ...) {
@@ -103,6 +105,18 @@ selectionchange.plot <- function(out.table, y=0, pch=25, col="black", bg="red", 
 	points(x=sel, y=y, pch=pch, col=col, bg=bg, ...)
 }
 
+# Average out all data tables from a directory
+mean.sim <- function(out.dir, max.reps=Inf, mc.cores=detectCores()-1) {
+	out.reps <- list.dirs(out.dir, full.names=TRUE, recursive=FALSE)
+	out.files <- list.files(pattern="out.*", path=out.reps, full.names=TRUE)
+
+	tt <- mclapply(out.files[1:(min(max.reps, length(out.files)))], read.table, header=TRUE, mc.cores=min(mc.cores, 4)) # read.tables on many cores is useless, probably limited by disk speed
+	ans <- replicate.mean(tt)
+	rm(tt)
+	gc()
+	return(ans)
+}
+
 # Phenotypic (expression) variation
 pheno.variation <- function(out.table) {
 	genes <- colnames(out.table)[grep(colnames(out.table), pattern="VPhen")]
@@ -114,4 +128,38 @@ molec.variation <- function(out.table) {
 	allvar <- colnames(out.table)[grep(colnames(out.table), pattern="VarAll")]
 	n.genes <- sqrt(length(allvar))
 	t(apply(out.table[,allvar], 1, function(x) { rowMeans(matrix(x, ncol=n.genes, byrow=TRUE)) }))
+}
+
+# Reaction norm (over a given time window)
+reaction.norm <- function(partial.out.table, signal.gene="MPhen1") {
+	.reac.norm <- function(x, signal) coef(lm(x ~ signal))[2]
+	expr <- partial.out.table[,grepl(pattern="MPhen", colnames(partial.out.table))]
+	sig <- expr[,signal.gene]
+	apply(expr[,colnames(expr) != signal.gene], 2, .reac.norm, signal=sig)
+}
+
+# Reaction norm dynamics (for a given window size)
+reaction.norm.dyn <- function(out.table, window.size=floor(nrow(out.table)/100), sliding=FALSE, signal.gene="MPhen1") {
+	stopifnot(window.size > 1, window.size <= nrow(out.table))
+	list.gens <- if (sliding) {
+					lapply(1:(nrow(out.table)-1), function(i.start) i.start:(i.start+window.size-1))
+				} else {
+					lapply(1:floor(nrow(out.table)/window.size), function(i.start) (1+(i.start-1)*window.size):(i.start*window.size))
+				}
+	ans <- do.call(rbind, lapply(list.gens, function(x) reaction.norm(out.table[x,], signal.gene=signal.gene)))
+	rownames(ans) <- as.character(sapply(list.gens, function(x) round(mean(out.table[x,"Gen"]))))
+	ans
+}
+
+# Average out all reaction norms from a directory (use FUN=abs to get the absolute value of the norm)
+mean.norm <- function(out.dir, max.reps=Inf, FUN=identity, mc.cores=detectCores()-1) {
+	out.reps <- list.dirs(out.dir, full.names=TRUE, recursive=FALSE)
+	out.files <- list.files(pattern="out.*", path=out.reps, full.names=TRUE)
+
+	tt <- mclapply(out.files[1:(min(max.reps, length(out.files)))], read.table, header=TRUE, mc.cores=min(mc.cores, 4)) # read.tables on many cores is useless, probably limited by disk speed
+	nn <- mclapply(tt, reaction.norm.dyn, window.size=window.size, sliding=sliding, mc.cores=mc.cores)
+	ans <- replicate.mean(lapply(nn, FUN))
+	rm(tt)
+	gc()
+	return(ans)
 }
