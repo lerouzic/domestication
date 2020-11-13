7ccfdbd4c888662b09b1b768fd12e789acc0a698
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-11-13 16:07:39 +0100

    Make it possible to plot absolute values of correlations in fig M.

--- results/figM.R
+++ results/figM.R
@@ -8,10 +8,12 @@ source("../src/analysis_networks.R")
 
 # Plotting correlations
 covtransf <- cov2cor # vs identity for covariances, but the color scale will not match
+absolute <- TRUE     # absolute value for correlations? 
 
 col.cor <- c("red", "white", "green") # for correlations -1, 0, 1
 
-meanG <- function(listG, transf=identity) {
+meanG <- function(listG) {
+	transf <- if(absolute) abs else identity
 	arr <- do.call(abind, c(lapply(listG, transf), list(along=3)))
 	rowMeans(arr, dims=2)
 }
@@ -37,7 +39,7 @@ Gmean.before.toplot <- Gmean.before.dom
 Gmean.after.toplot  <- Gmean.after.dom
 
 ng <- ncol(Gmean.before.toplot)-1
-cols <- colorRampPalette(col.cor)(1001)
+cols <- colorRampPalette(col.cor[(if absolute 2 else 1):length(col.col))(1001)
 cex.axis <- 0.6
 
 pdf("figM.pdf", width=9, height=4)
@@ -46,17 +48,19 @@ layout(t(1:3), width=c(0.1, 0.45, 0.45))
 
 par(mar=c(1, 4, 4, 0.1))
 
-image(y=seq(-1, 1, length.out=length(cols)), z=t(as.matrix(seq(-1, 1, length.out=length(cols)-1))), zlim=c(-1,1), col=cols, xaxt="n", xlab="", ylab="Genetic correlation")
+st <- if (absolute) 0 else -1
+yl <- paste0(if (absolute) "|" else "", "Genetic correlation", if (absolute) "|" else "")
+image(y=seq(st, 1, length.out=length(cols)), z=t(as.matrix(seq(st, 1, length.out=length(cols)-1))), zlim=c(st,1), col=cols, xaxt="n", xlab="", ylab=yl)
 
 par(mar=c(0.1, 2, 4, 0.1))
 
-image(x=1:ng, y=1:ng, t(Gmean.before.toplot[(ng+1):2,2:(ng+1)]), axes=FALSE, xlab="", ylab="", zlim=c(-1, 1), col=cols)
+image(x=1:ng, y=1:ng, t(Gmean.before.toplot[(ng+1):2,2:(ng+1)]), axes=FALSE, xlab="", ylab="", zlim=c(st, 1), col=cols)
 title("Before domestication")
 axis(2, at=1:ng, rev(sel.before.dom[-1]), cex.axis=cex.axis, tick=FALSE, line=-1)
 axis(3, at=1:ng, sel.before.dom[-1], cex.axis=cex.axis, tick=FALSE, line=-1)
 
 
-image(x=1:ng, y=1:ng, t(Gmean.after.toplot[(ng+1):2,2:(ng+1)]), axes=FALSE, xlab="", ylab="", zlim=c(-1, 1), col=cols)
+image(x=1:ng, y=1:ng, t(Gmean.after.toplot[(ng+1):2,2:(ng+1)]), axes=FALSE, xlab="", ylab="", zlim=c(st, 1), col=cols)
 title("After domestication")
 axis(2, at=1:ng, rev(sel.after.dom[-1]), cex.axis=cex.axis, tick=FALSE, line=-1)
 axis(3, at=1:ng, sel.after.dom[-1], cex.axis=cex.axis, tick=FALSE, line=-1)
