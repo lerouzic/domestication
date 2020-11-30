ad9393d68c0ca1b9b8931621914164dc5a3111e6
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-11-30 16:59:24 +0100

    New fig S8: comparison of two strenghts of selection.

---
+++ results/figS8.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+source("./common-par.R")
+
+s <- c(blue=10, red=50)
+theta <- 0.5
+
+pdf("figS8.pdf", width=panel.width, height=panel.height)
+	par(mar=mar.notitle)
+	
+	plot(NULL, xlab="Gene expression", ylab="Fitness", xlim=c(0,1), ylim=c(0,1))
+	
+	for (ns in names(s)) {
+		curve(exp(-s[ns]*(x-theta)^2), add=TRUE, xlim=c(0,1),lty=1, col=ns) 
+	}
+	
+	legend("topright", lty=1, col=names(s), legend=paste0("s=", s), cex=cex.legend, bty="n")
+dev.off()
