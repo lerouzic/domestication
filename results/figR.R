9054ba7c36b45dceb993b72184c1b24450545ed4
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-12-01 22:11:28 +0100

    Cosmetic changes in figR.

--- results/figR.R
+++ results/figR.R
@@ -7,10 +7,10 @@ source("./common-fig.R")
 pdf("figR.pdf", width=panel.width, height=panel.height)
 	par(mar=mar.notitle)
 	
-	plot.evol.gene("default", xaxt="n")
+	plot.evol.gene("default", xaxt="n", ylim=c(0,0.25))
 	
-	bottleneck.plot(Ndyn.all[["default"]], y=0, lwd=2)
-	selectionchange.plot(meansim.all[["default"]], y=0, cex=1.5)
+	bottleneck.plot(Ndyn.all[["default"]], y=0.24, lwd=2)
+	selectionchange.plot(meansim.all[["default"]], y=0.24, cex=1.5)
 	generation.axis()
 
 dev.off()
