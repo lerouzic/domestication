b2b770263e6e1c5e97c5583ce3ee610b0c1c16d5
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2021-01-28 16:33:42 +0100

    Labels and scale in fig SS2.

--- results/figSS2.R
+++ results/figSS2.R
@@ -12,7 +12,7 @@ dG <- 1000
 pdf("figSS2a.pdf", width=panel.width, height=panel.height)
 	par(mar=mar.notitle)
 	
-	plot.network.feature(scenarios, what="path", xaxt="n", deltaG=dG)
+	plot.network.feature(scenarios, what="path", xaxt="n", deltaG=dG, ylab="Mean path length", ylim=c(2.4, 2.7))
 	
 	legend("bottomright", lty=lty.sce[scenarios], col=col.sce[scenarios], legend=legname(scenarios), cex=cex.legend, bty="n")
 	
@@ -24,7 +24,7 @@ dev.off()
 pdf("figSS2b.pdf", width=panel.width, height=panel.height)
 	par(mar=mar.notitle)
 	
-	plot.network.feature(scenarios, what="diameter", xaxt="n", deltaG=dG)
+	plot.network.feature(scenarios, what="diameter", xaxt="n", deltaG=dG, ylab="Network diameter", ylim=c(5.5, 6.5))
 	
 	legend("bottomright", lty=lty.sce[scenarios], col=col.sce[scenarios], legend=legname(scenarios), cex=cex.legend, bty="n")
 	
@@ -36,7 +36,7 @@ dev.off()
 pdf("figSS2c.pdf", width=panel.width, height=panel.height)
 	par(mar=mar.notitle)
 	
-	plot.network.feature(scenarios, what="clusters", xaxt="n", deltaG=dG)
+	plot.network.feature(scenarios, what="clusters", xaxt="n", deltaG=dG, ylab="Number of clusters", ylim=c(6, 12))
 	
 	legend("bottomright", lty=lty.sce[scenarios], col=col.sce[scenarios], legend=legname(scenarios), cex=cex.legend, bty="n")
 	
@@ -48,7 +48,7 @@ dev.off()
 pdf("figSS2d.pdf", width=panel.width, height=panel.height)
 	par(mar=mar.notitle)
 	
-	plot.network.feature(scenarios, what="centralization", xaxt="n", deltaG=dG)
+	plot.network.feature(scenarios, what="centralization", xaxt="n", deltaG=dG, ylab="Network centralization", ylim=c(0.12, 0.14))
 	
 	legend("bottomright", lty=lty.sce[scenarios], col=col.sce[scenarios], legend=legname(scenarios), cex=cex.legend, bty="n")
 	
@@ -60,7 +60,7 @@ dev.off()
 pdf("figSS2e.pdf", width=panel.width, height=panel.height)
 	par(mar=mar.notitle)
 	
-	plot.network.feature(scenarios, what="modularity", xaxt="n", deltaG=dG)
+	plot.network.feature(scenarios, what="modularity", xaxt="n", deltaG=dG, ylab="Modularity", ylim=c(0.2, 0.35))
 	
 	legend("bottomright", lty=lty.sce[scenarios], col=col.sce[scenarios], legend=legname(scenarios), cex=cex.legend, bty="n")
 	
