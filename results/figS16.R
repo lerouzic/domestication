de9ef4b3e85cbb8c4c867a0f480078dcd2941a03
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2021-07-06 17:44:30 +0200

    Reorganization of figS16.

--- results/figS16.R
+++ results/figS16.R
@@ -3,13 +3,14 @@ source("../src/analysis_networks.R")
 
 # Toy example of the mutational process
 
-col.genes <- c("green", "blue", "orange", "purple")
+col.genes <- c("darkgreen", "black", "black", "black")
 names.genes <- c("E", "A", "B", "C")
-reg.mag <- 0.4
-col.lim <- c(-reg.mag, reg.mag)
+env.default <- 0.5
+env.alt     <- 0.8
+col.lim <- c(-0.5, 0.5)
 
-net.plot <- function(W, xlim=c(-0.2, 1.2), ylim=c(-0.2, 1.2), col=c("red", "white", "black"), text.cex=1.6, shft=0.2) {
-	opar <- par(mar=c(1,1,1,1))
+net.plot <- function(W, xlim=c(-0.2, 1.2), ylim=c(-0.2, 1.2), col=c("black", "white", "black"), text.cex=1.6, shft=0.2, bold=numeric(0)) {
+	opar <- par(mar=c(3,1,1,1))
 	stopifnot(ncol(W) == 4, nrow(W) == 4)
 	rr <- colorRamp(col)
 	W[W < col.lim[1]] <- col.lim[1]
@@ -27,6 +28,8 @@ net.plot <- function(W, xlim=c(-0.2, 1.2), ylim=c(-0.2, 1.2), col=c("red", "whit
 			if (i==j) next
 			cc <- coords.genes[c(i,j),]
 			ww <- W[j,i]
+			myc <- (i-1)*ncol(W)+j
+			bb <- length(bold) > 0 &&  myc %in% bold
 			dd <- 0.03 * if (i > j) 1 else -1
 			if (ww != 0)
 				arrows( x0=cc[1,1]+shft*diff(cc[,1]) + dd, 
@@ -34,22 +37,36 @@ net.plot <- function(W, xlim=c(-0.2, 1.2), ylim=c(-0.2, 1.2), col=c("red", "whit
 						y0=cc[1,2]+shft*diff(cc[,2]) + dd,
 						y1=cc[2,2]-shft*diff(cc[,2]) + dd,
 						lwd=3, angle=if(ww < 0) 90 else 45, length=0.15,
-						col=rgb(rr((ww-col.lim[1])/diff(col.lim))/255))
+						col=if (bb) "red" else rgb(rr((ww-col.lim[1])/diff(col.lim))/255))
 		}
 	}
 	par(mar=opar)
 }
 
-W.plot <- function(W, xlim=c(-0.2, 1.2), ylim=c(-0.2, 1.2), col=c("red", "white", "black"), cex.text=1.6, main="", bold=numeric(0), brackets=TRUE) {
+W.plot <- function(W, xlim=c(-0.2, 1.2), ylim=c(-0.2, 1.2), col=c("black", "black", "black"), cex.text=1.6, main="", bold=numeric(0), brackets=TRUE, show.zeros=FALSE, first.line=FALSE) {
 	stopifnot(ncol(W) == 4, nrow(W) == 4)
 	rr <- colorRamp(col)
 	plot(NULL, xlim=xlim, ylim=ylim, axes=FALSE, xlab="", ylab="", main=main)
 	
+	if (!first.line) {
+		Wb <- matrix(FALSE, ncol=ncol(W), nrow=nrow(W))
+		Wb[bold] <- TRUE
+		bold <- which(Wb[-1,])
+		W <- W[-1,]
+	}
+	
 	for (i in 1:nrow(W)) {
 		for (j in 1:ncol(W)) {
-			myc <- (j-1)*ncol(W)+i
+			myc <- (j-1)*nrow(W)+i
 			bb <- length(bold) > 0 &&  myc %in% bold
-			text(x=(j-1)/(ncol(W)-1), y=1-(i-1)/(ncol(W)-1), as.character(W[i,j]), col=rgb(rr((W[i,j]-col.lim[1])/diff(col.lim))/255), cex=0.8*cex.text, font=if(bb) 2 else 1)
+			if (W[i,j] != 0 || show.zeros)
+				text(
+					x=(j-1)/(ncol(W)-1), 
+					y=1-(i-1)/(nrow(W)-1), 
+					as.character(W[i,j]), 
+#~ 					col=rgb(rr((W[i,j]-col.lim[1])/diff(col.lim))/255), font=if(bb) 2 else 1
+					cex=0.8*cex.text, col=if(bb) "red" else "black"
+			)
 		}
 	}
 	
@@ -61,13 +78,13 @@ W.plot <- function(W, xlim=c(-0.2, 1.2), ylim=c(-0.2, 1.2), col=c("red", "white"
 	}
 	
 	# No way to have different axis colors without cheating
-	for (i in 1:ncol(W))
-		axis(2, at=rev(seq(0, 1, length.out=nrow(W)))[i], labels=rownames(W)[i], tick=FALSE, lty=0, cex.axis=cex.text, col.axis=col.genes[i], las=2)
+	for (i in 1:nrow(W))
+		axis(2, at=rev(seq(0, 1, length.out=nrow(W)))[i], labels=rownames(W)[i], tick=FALSE, lty=0, cex.axis=cex.text, col.axis=col.genes[i+if(first.line) 0 else 1], las=2)
 	for (j in 1:ncol(W))
 		axis(3, at=seq(0, 1, length.out=ncol(W))[j], labels=colnames(W)[j], tick=FALSE, lty=0, cex.axis=cex.text, col.axis=col.genes[j])
 }
 
-dyn.plot <- function(W, env=0.5, main="") {
+dyn.plot <- function(W, env=env.default, main="", gene.names=TRUE) {
 	mm <- model.M2(W=W, steps=16, env=env, full=TRUE)
 	
 	plot(NULL, xlim=c(0, 16), ylim=c(0, 1), xlab="Time steps", ylab="Gene expression", bty="n", main=main)
@@ -75,23 +92,27 @@ dyn.plot <- function(W, env=0.5, main="") {
 	polygon(x=c(12, 16, 16, 12), y=c(0,0,1,1), border=NA, col="lightgray")
 
 	for (i in 1:nrow(W)) {
-		lines(x=0:16, mm$full[i,], col=col.genes[i], lwd=2)
+		lines(x=0:(ncol(mm$full)-1), mm$full[i,], col=col.genes[i], lwd=2)
+		if (gene.names) 
+			text(ncol(mm$full)-1, mm$full[i,ncol(mm$full)], rownames(W)[i], col=col.genes[i], pos=4)
 	}
-	
 }
 
 fit.plot <- function(thetas, s=rep(10, length(thetas)), phen=rep(NA, length(thetas)), xlab="Fitness", ylab="Gene expression", ...) {
-	inv.gauss <- function(theta, s, reverse=FALSE) {
+	inv.gauss <- function(theta, s, reverse=FALSE, yrange = c(0,1)) {
 	function(y) {
-	if (reverse)
+	ans <- if (reverse)
 		theta + sqrt(-s*log(y))/s
 	else
 		theta - sqrt(-s*log(y))/s
+	ans[ans < yrange[1]] <- NA
+	ans[ans > yrange[2]] <- NA
+	ans
 	}}
 	
-	opar <- par(mar=c(par("mar")[1], 0, par("mar")[3:4]))
+	opar <- par(mar=c(par("mar")[1], 0, par("mar")[3], 2))
 	
-	plot(NULL, xlim=c(0,1), ylim=c(0,1), xlab=xlab, ylab=ylab, bty="n", xaxt="n", ...)
+	plot(NULL, xlim=c(0,1.1), ylim=c(0,1), xlab=xlab, ylab=ylab, bty="n", xaxt="n", ...)
 	axis(1, at=c(0,1/2,1), labels=c("0", "0.5", "1"))
 	abline(h=c(0,1), col="darkgray")
 	for (i in seq_along(thetas)) {
@@ -101,36 +122,45 @@ fit.plot <- function(thetas, s=rep(10, length(thetas)), phen=rep(NA, length(thet
 		curve(inv.gauss(thetas[i], s[i])(x), add=TRUE, col=col.genes[i], lwd=2)
 		curve(inv.gauss(thetas[i], s[i], TRUE)(x), add=TRUE, col=col.genes[i], lwd=2)
 		if (!is.na(phen[i]))
-		abline(h=phen[i], col=col.genes[i], lty=2)
+			lines(x=c(-0.3, 1), y=rep(phen[i],2), col=col.genes[i], lty=2, xpd=NA)
+		points(rep(1, length(thetas)), thetas, pch=19, col="blue", cex=2)
+		text(rep(1, length(thetas)), thetas, bquote(theta[.(names.genes[i])]), col="blue", pos=4, xpd=NA)
 	}
 	par(mar=opar)
 }
 
 
-thetas <- c(NA, 0.7, 0.3, NA)
+thetas <- c(NA, 0.6, NA, NA)
 
-W.before <- W.after <- reg.mag*rbind(
+W.before <- W.after <- rbind(
 	c( 0, 0, 0, 0),
-    c( 1, 0,  0, 0.25),
-    c(-1, 1,  0, 0),
-    c( 1, 0,  0, 0))
-W.after[2,4] <- W.after[2,4]*4
+    c( 0.4, 0, -0.1, 0),
+    c( 0, 0,  0, 0.5),
+    c( 0, 0,  0, 0))
+W.after[2,4] <- 0.6
 rownames(W.before) <- rownames(W.after) <- colnames(W.before) <- colnames(W.after) <- names.genes
       
 
-pdf("figS16.pdf", width=12, height=6)
-layout(matrix(1:8, byrow=TRUE, ncol=4), widths=c(0.2,0.2,0.4,0.1))
+pdf("figS16.pdf", width=12, height=9)
+layout(matrix(c(1:5,0,6:11), byrow=TRUE, ncol=4), widths=c(0.2,0.2,0.4,0.1))
 
-par(cex=0.8)
+par(cex=0.8, oma=c(0, 5, 0, 0))
 
 W.plot(W.before)
+mtext(side=2, text="Original\nNetwork", line=4, cex=2)
 net.plot(W.before)
-dyn.plot(W.before, main="Original network")
-fit.plot(thetas, ylab="", yaxt="n", phen=model.M2(W.before)$mean)
+dyn.plot(W.before, env=env.default)
+fit.plot(thetas, ylab="", yaxt="n", phen=model.M2(W.before, env=env.default)$mean)
 
+plot(NULL,xlim=0:1, ylim=0:1,axes=FALSE,ann=FALSE)
+mtext(side=2, text="Environment\nchange", line=4, cex=2)
+dyn.plot(W.before, env=env.alt)
+fit.plot(thetas, ylab="", yaxt="n", phen=model.M2(W.before, env=env.alt)$mean)
 
 W.plot(W.after, bold=which(W.before != W.after))
-net.plot(W.after)
-dyn.plot(W.after, main="Mutant network")
-fit.plot(thetas, ylab="", yaxt="n", phen=model.M2(W.after)$mean)
+mtext(side=2, text="Mutant\nNetwork", line=4, cex=2)
+
+net.plot(W.after, bold=which(W.before != W.after))
+dyn.plot(W.after, env=env.default)
+fit.plot(thetas, ylab="", yaxt="n", phen=model.M2(W.after, env=env.default)$mean)
 dev.off()
