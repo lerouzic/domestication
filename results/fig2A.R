96e442714e58bef864227795bd3140e1fb689b71
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2021-07-08 16:11:51 +0200

    Change in the meaning of fig2A (three scenarios instead of gene by gene).

--- results/fig2A.R
+++ results/fig2A.R
@@ -4,21 +4,21 @@
 
 source("./common-fig.R")
 
+scenarios <- c("default", "nobot", "noselc")
+
 pdf("fig2A.pdf", width=panel.width, height=panel.height)
 	par(mar=mar.notitle)
 	
 	sel.pat <- substr(selpattern.all[["default"]], 1, 1)
 	sel.pat[sel.pat == "c"] <- "s" # No need to distinguish constant and stable
 	
-	plot.evol.gene("default", xaxt="n", ylim=c(0,0.25))
+	plot.evol(scenarios, xaxt="n", ylim=c(0,0.15))
 	
 	bottleneck.plot(Ndyn.all[["default"]], y=0.24, lwd=2)
 	selectionchange.plot(meansim.all[["default"]], y=0.24, cex=1.5)
 	generation.axis()
 	
-	legend("topright", lty=c(0, lty.sel[unique(sel.pat)]), col=c(0,1,1,1), legend=c("Before dom:", "Stable","Plastic", "Non-selected"), cex=cex.legend, bty="n")
-	legend("right", lty=c(0,1,1,1), col=c(0, col.sel[unique(sel.pat)]), legend=c("After dom:", "Stable","Plastic", "Non-selected"), cex=cex.legend, bty="n")
-
+	legend("topright", lty=lty.sce[scenarios], col=col.sce[scenarios], legend=legname(scenarios), bty="n", cex=cex.legend)
 
 	subpanel("A")
 dev.off()
