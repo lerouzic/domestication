e1324d8dbb1176f300ed8d02eeb1bf33e8d5421f
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2021-07-13 17:00:42 +0200

    Fig 5A now displays the total number of connections.

--- results/fig5A.R
+++ results/fig5A.R
@@ -11,13 +11,13 @@ scenarios <- c("default", "nobot", "noselc")
 pdf("fig5A.pdf", width=panel.width, height=panel.height)
 	par(mar=mar.notitle)
 	
-	plot.inout.gainloss(scenarios, deltaG=deltaG, ylim=c(-20, 20), xaxt="n")
-	
-	legend("topright", lty=c(1,1,lty.sce[scenarios]), col=c(col.gl, rep("black", length(scenarios))), legend=legname(c(names(col.gl), scenarios)), cex=cex.legend, bty="n")
-	
+	plot.nconn(scenarios, ylim=c(20, 60), xaxt="n")
+
 	generation.axis()
-	bottleneck.plot(Ndyn.all[["default"]], y=-19.5, lwd=2)
-	selectionchange.plot(meansim.all[["default"]], y=-19.5, cex=1.5)
+	bottleneck.plot(Ndyn.all[["default"]], y=40, lwd=2)
+	selectionchange.plot(meansim.all[["default"]], y=40, cex=1.5)
+
+	legend("bottomright", lty=lty.sce[scenarios], col=col.sce[scenarios], legend=legname(scenarios), cex=cex.legend, bty="n")
 
 	subpanel("A")
 dev.off()
